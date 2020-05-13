4467e6e3986a5aceee9a3ad9a32d502a6b161849
Camilo Vargas <kmilovargas913@gmail.com>
2020-05-12 22:59:23 -0500

    init

---
+++ src/main/java/udec/ucundi/edu/disquera/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package udec.ucundi.edu.disquera;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
