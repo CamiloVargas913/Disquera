c5de4501bc3402b1c3af7ee602838ff00a6cd05d
Camilo Vargas <kmilovargas913@gmail.com>
2020-05-12 23:21:43 -0500

    clase usuario

---
+++ src/main/java/udec/ucundi/edu/bean/Index.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udec.ucundi.edu.bean;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author PROFESIONAL
+ */
+@Named(value = "index")
+@RequestScoped
+public class Index {
+
+    /**
+     * Creates a new instance of Index
+     */
+    public Index() {
+        
+    }
+    
+}
